219a4e1caf8b1122b76faa361cf4ed695e0f1689
BuenoOtavio <programador.bueno@gmail.com>
2016-12-05 16:02:55 -0200

    Removido mapeamento do Jquery implementado

--- mapDom.js
+++ mapDom.js
@@ -4,84 +4,83 @@
         i,
         arg,
         msg,
+        hasOnw,
         obj;
 
-    MapDom.prototype.jquery = mapJquery;
     MapDom.prototype.javascript = mapJavascript;
-    MapDom.prototype.isNullorEmpty = isNullorEmpty;    
+    MapDom.prototype.isNullorEmpty = isNullorEmpty;
 
     function MapDom() {
 
         arg = arguments;
         for (var name in arg) {
-            
             property = arg[name];
-            
-           if(isArray(property)){
-               resetWhile(property);
 
-               while (i < len) {
+            if (isArray(property)) {
+                resetWhile(property);
 
-                   this[property[i]] = property[i];
+                while (i < len) {
 
-                   increase();
-               };
-           }else{
-               this[property] = property;
-           }
+                    this[property[i]] = property[i];
+
+                    increase();
+                };
+            } else {
+                this[property] = property;
+            }
         }
     };
-    function mapJquery(param) {
-        if (param !== undefined) {
-            if (checkProperty.call(this, param)) {
-                this[param] = $("#" + param);
-            }
-            this[param] = $("#" + param);
-        } else {
-            for (var name in this) {
-                if (checkProperty.call(this, name)) {
-                    this[name] = $("#" + name);
-                }          
-            };
-        }      
-    };
     function mapJavascript(param) {
+        hasOnw = Object.prototype.hasOwnProperty;
         if (param !== undefined) {
-            if (checkProperty.call(this, param)) {
+            if (isArray(param)) {
+                resetWhile(param);
+                while (i < len) {
+                    if (checkProperty(this, param[i])) {
+                        this[param[i]] = document.getElementById(param[i]);
+                    }
+                    increase();
+                }
+            }
+            if (checkProperty(this, param)) {
                 this[param] = document.getElementById(param);
             }
         } else {
             for (var name in this) {
-                if (checkProperty.call(this,name)) {
+                if (checkProperty(this, name)) {
                     this[name] = document.getElementById(name);
-                }       
+                }
             };
-        }       
+        }
     };
 
     function isNullorEmpty() {
 
         msg = [];
-
+        hasOnw = Object.prototype.hasOwnProperty;
         for (name in this) {
 
-            property = this[name];
+            if (checkProperty(this, name)) {
 
-            if (isArray(property)) {
+                property = this[name];
 
-                if (!validate(property.join(","))) {
+                if (isArray(property)) {
+
+                    if (!validate(property.join(","))) {
+
+                        msg.push(name);
+                    }
+
+                } else if (!validate(property)) {
 
                     msg.push(name);
                 }
 
-            } else if (!validate(property)) {
+            }
 
-                msg.push(name);
-            }        
+            return msg;
         }
-
-        return msg;
-    };
+    }
     function resetWhile(objeto) {
         len = objeto.length;
         i = 0;
@@ -89,23 +88,13 @@
     };
 
     function increase() {
-        i++;
+        i += 1;
     };
 
     function decrease() {
-        i = i - 1;
+        i -= 1;
     };
 
-    function checkProperty(name) {
-        switch (name) {
-            case "jquery":
-            case "javascript":
-            case "isNullorEmpty":
-                return false;
-            default:               
-                return this.hasOwnProperty(name);
-        }
-    };
     function validate(param) {
         if (typeof param === "string") {
             param = param.trim();
@@ -123,6 +112,10 @@
     function isArray(obj) {
         return Object.prototype.toString.call(obj) === '[object Array]';
     };
+
+    function checkProperty(obj, param) {
+        return hasOnw.call(obj, param);
+    }
 
     root.MapDom = MapDom;
 
