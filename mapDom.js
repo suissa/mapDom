7e2c84fc8746bc9dcbcd58dc28edd820c00b9507
Jean Carlo Nascimento <jnascimento@gmail.com>
2016-12-05 17:16:55 -0200

    etapa 1

--- mapDom.js
+++ mapDom.js
@@ -1,122 +1,102 @@
 (function (root) {
-    var property,
-        len,
-        i,
-        arg,
-        msg,
-        hasOnw,
-        obj;
+  var property,
+    len,
+    i,
+    arg,
+    msg,
+    hasOnw,
+    obj;
 
-    MapDom.prototype.javascript = mapJavascript;
-    MapDom.prototype.isNullorEmpty = isNullorEmpty;
+  MapDom.prototype.javascript = mapJavascript;
+  MapDom.prototype.isNullorEmpty = isNullorEmpty;
 
-    function MapDom() {
+  function MapDom() {
 
-        arg = arguments;
-        for (var name in arg) {
-            property = arg[name];
+    arg = arguments;
+    for (var name in arg) {
+      property = arg[name];
 
-            if (isArray(property)) {
-                resetWhile(property);
-
-                while (i < len) {
-
-                    this[property[i]] = property[i];
-
-                    increase();
-                };
-            } else {
-                this[property] = property;
-            }
-        }
-    };
-    function mapJavascript(param) {
-        hasOnw = Object.prototype.hasOwnProperty;
-        if (param !== undefined) {
-            if (isArray(param)) {
-                resetWhile(param);
-                while (i < len) {
-                    if (checkProperty(this, param[i])) {
-                        this[param[i]] = document.getElementById(param[i]);
-                    }
-                    increase();
-                }
-            }
-            if (checkProperty(this, param)) {
-                this[param] = document.getElementById(param);
-            }
-        } else {
-            for (var name in this) {
-                if (checkProperty(this, name)) {
-                    this[name] = document.getElementById(name);
-                }
-            };
-        }
-    };
-
-    function isNullorEmpty() {
-
-        msg = [];
-        hasOnw = Object.prototype.hasOwnProperty;
-        for (name in this) {
-
-            if (checkProperty(this, name)) {
-
-                property = this[name];
-
-                if (isArray(property)) {
-
-                    if (!validate(property.join(","))) {
-
-                        msg.push(name);
-                    }
-
-                } else if (!validate(property)) {
-
-                    msg.push(name);
-                }
-
-            }
-
-            return msg;
-        }
+      if (isArray(property)) {
+        // resetWhile(property);
+        // len = property.length;
+        var i = 0;
+        // obj = property;
+        while (i < property.length) {
+          this[property[i]] = property[i];
+          ++i;
+          // increase(i);
+        };
+      } else {
+        this[property] = property;
+      }
     }
-    function resetWhile(objeto) {
+  };
+
+  function mapJavascript(param) {
+    hasOnw = Object.prototype.hasOwnProperty;
+    if (param && param !== 0) {
+      if (isArray(param)) {
+        // resetWhile(param);
         len = objeto.length;
         i = 0;
-        obj = objeto;
-    };
-
-    function increase() {
-        i += 1;
-    };
-
-    function decrease() {
-        i -= 1;
-    };
-
-    function validate(param) {
-        if (typeof param === "string") {
-            param = param.trim();
+        // obj = objeto;
+        while (i < param.length) {
+          if (checkProperty(this, param[i])) {
+            this[param[i]] = document.getElementById(param[i]);
+          }
+          i++;
         }
-        switch (param) {
-            case null:
-            case "undefined":
-            case "":
-                return false;
-
-            default:
-                return true;
+      }
+      if (checkProperty(this, param)) {
+        this[param] = document.getElementById(param);
+      }
+    } else {
+      for (var name in this) {
+        if (checkProperty(this, name)) {
+          this[name] = document.getElementById(name);
         }
+      };
     }
-    function isArray(obj) {
-        return Object.prototype.toString.call(obj) === '[object Array]';
-    };
+  };
 
-    function checkProperty(obj, param) {
-        return hasOnw.call(obj, param);
+  function isNullorEmpty() {
+
+    msg = [];
+    hasOnw = Object.prototype.hasOwnProperty;
+    for (name in this) {
+      if (checkProperty(this, name)) {
+        property = this[name];
+        if (isArray(property)) {
+          if (!validate(property.join(","))) {
+            msg.push(name);
+          }
+        } else if (!validate(property)) {
+          msg.push(name);
+        }
+      }
+
+      return msg;
     }
+  }
 
-    root.MapDom = MapDom;
+  const resetWhile = (objeto) => {
+    len = objeto.length;
+    i = 0;
+    obj = objeto;
+  };
+
+  const increase = (i) => i += 1;
+
+  const decrease = (i) => i -= 1;
+
+  const validate = (param) => (typeof param === "string" 
+                                && (param.trim() !== "" ||
+                                    param.trim() !== "undefined"))  
+
+  const isArray = (obj) => Object.prototype.toString.call(obj) === '[object Array]';
+
+  const checkProperty = (obj, param) => hasOnw.call(obj, param);
+
+  root.MapDom = MapDom;
 
 })(this)
